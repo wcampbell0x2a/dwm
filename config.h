6e45a50932f5f73ae2c6df3613e3005a351ac995
userWayneCampbell <wcampbell1995@gmail.com>
2018-10-01 10:22:02 -0400

    Change terminal to kitty
    Removed gnome-terminal, wanted a terminal that used config files. This
    terminal looks interesting.

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "dbus-launch", "gnome-terminal", "--zoom=0.8", NULL };
+static const char *termcmd[]  = { "kitty"};
 static const char *lockscreen[] = {"slock", "-m I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux..."};
 
 static Key keys[] = {
